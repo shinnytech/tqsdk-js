707c367aeeeede7e30470beee27ffae8aa803e69
yangyang <yangyang@shinnytech.com>
2017-08-24 11:08:36 +0800

    增加指标类生成描述object代码原型

--- js/tamanager.js
+++ js/tamanager.js
@@ -1,42 +1,92 @@
-var ta_instance_map = {};
-
-    function SUM2(func, N, P) {
-        var v = 0;
-        for (var i = 0; i < N; i++) {
-            if (P > i) {
-                v = v + func(P - i)
-            }
-        }
-        return v;
-    }
-
-//atyle2---------------------------------------------------------------------------
-//BR: SUM(MAX(0, HIGH - REF(CLOSE, 1)), N) / SUM(MAX(0, REF(CLOSE, 1) - LOW), N) * 100;//取最高价减去一个周期前的收盘价的与0中的最大值，求和，取一个周期前的收盘价减去最低价与0中的最大值，求和，两个和的百分比
-    function br2(shigh, slow, sclose, n, P) {
-        return SUM2(function (P) {
-                return Math.max(shigh(P) - sclose(P - 1), 0);
-            }, n, P)
-            / SUM2(function (P) {
-                return Math.max(sclose(P - 1) - slow(P), 0);
-            }, n, P);
-    }
-
-    function arbr2(C) {
-//        C.OUT("BR", br2(C.IN("HIGH"), C.IN("LOW"), C.IN("CLOSE"), C.PARAM("N"), C.P));
-        C.OUT("BR", C.IN("HIGH")(C.P) + 5);
-    };
-
+// ---------------------------------------------------------------------------
+function ma(C) {
+    C.OUT("BR", C.IN("HIGH")(C.P) + C.PARAM("N"));
+};
+function macd(C) {
+    C.IN("CLOSE").setMemo("this is close");
+    C.OUT("BR", C.IN("CLOSE")(C.P) + C.PARAM("N"));
+};
+var ta_funcs = [ma];
 // ---------------------------------------------------------------------------
 
+function ParamDefine(name){
+    var f = function(){return 123};
+    f.sname = name;
+    f.memo = "";
+    f.default_value = NaN;
+    f.min_value = NaN;
+    f.max_value = NaN;
+    f.setMemo = function(memo){
+        f.memo = memo;
+        return f;
+    };
+    f.setDefault = function(default_value){
+        f.default_value = default_value;
+        return f;
+    };
+    return f;
+}
+
+function InputSerialDefine(name) {
+    var f = function(){return 123};
+    f.sname = name;
+    f.memo = "";
+    f.selector = name;
+    f.setMemo = function(memo){
+        f.memo = memo;
+        return f;
+    };
+    return f;
+};
+
+function OutputSerialDefine(name) {
+    var f = function(){return 123};
+    f.sname = name;
+    f.memo = "";
+    f.style = 0;
+    f.color = 0;
+    f.width = 1;
+    f.yaxis = 0;
+    f.setMemo = function(memo){
+        f.memo = memo;
+        return f;
+    };
+    f.setStyle = function(style){
+        f.style = style;
+        return f;
+    };
+    f.setColor = function(color){
+        f.style = color;
+        return f;
+    };
+    f.setWidth = function(width){
+        f.style = width;
+        return f;
+    };
+    f.setAxis = function(axis){
+        f.style = axis;
+        return f;
+    };
+    return f;
+};
+
+
+var ta_instance_map = {};
+var ta_class_map = {};
 
 (function () {
 
     function tm_init() {
         //todo: 初始化
+        //更新所有指标类定义, 并发送到主进程
+        for (var i = 0; i < ta_funcs.length; i++) {
+            var f = ta_funcs[i];
+            tm_update_class_define(f);
+        }
         ta_instance_map[1] = {
             "instance_id": 1,
             "epoch":	3,
-            "func": arbr2,
+            "func": ma,
             "params": {
                 "N": 3123,
             },
@@ -107,6 +157,36 @@ var ta_instance_map = {};
             }
         */
     }
+    function tm_update_class_define(ta_func){
+        let indicator_name = ta_func.name;
+        ta_class_map[indicator_name] = {
+            "name": indicator_name,
+            "params": [],
+            "input_serials": [],
+            "output_serials": [],
+        };
+        let class_define = ta_class_map[indicator_name];
+        context_define = {
+            "P": 0,
+            "PARAM": function (param_name) {
+                var param_define = ParamDefine(param_name);
+                class_define.params.push(param_define);
+                return param_define;
+            },
+            "IN": function (serial_selector) {
+                var input_serial_define = InputSerialDefine(serial_selector);
+                class_define.input_serials.push(input_serial_define);
+                return input_serial_define;
+            },
+            "OUT": function(serial_name, value){
+                var output_serial_define = OutputSerialDefine(serial_name);
+                class_define.output_serials.push(output_serial_define);
+                return output_serial_define;
+            },
+        }
+        ta_func(context_define);
+        console.log("finish define");
+    }
 
     function tm_set_indicator_class_list() {
     }
@@ -156,7 +236,7 @@ var ta_instance_map = {};
         console.log("data_left" + data_left);
         console.log("data_right" + data_right);
         for (var i = data_left; i <= data_right; i++) {
-            console.log("calc:" + i);
+            // console.log("calc:" + i);
             context_calc["P"] = i;
             ta_instance["func"](context_calc);
         }
