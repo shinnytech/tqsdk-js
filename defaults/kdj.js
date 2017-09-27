e2c00372a639740487b21de1218677cabc041b10
yangyang <yangyang@shinnytech.com>
2017-09-27 17:35:05 +0800

    修正默认技术指标

--- defaults/kdj.js
+++ defaults/kdj.js
@@ -1,23 +1,25 @@
 function* kdj(C){
+    //指标定义
     C.DEFINE({
         type: "SUB",
         cname: "KDJ",
-        memo: "",
         state: "KLINE",
     });
+    //参数
     let n = C.PARAM(3, "N");
     let m1 = C.PARAM(5, "M1");
     let m2 = C.PARAM(5, "M2");
+    //输入序列
     let close = C.SERIAL("CLOSE");
     let high = C.SERIAL("HIGH");
     let low = C.SERIAL("LOW");
-
+    //输出序列
     let k = C.OUTS("LINE", "k", {color: RED});
     let d = C.OUTS("LINE", "d", {color: GREEN});
     let j = C.OUTS("LINE", "j", {color: YELLOW});
-
+    //临时序列
     let rsv = [];
-
+    //计算
     while(true) {
         let i = yield;
         let hv = HIGHEST(i, high, n);
--- defaults/macd.js
+++ defaults/macd.js
@@ -1,7 +1,5 @@
 function* macd(C) {
-    // DIFF : EMA(CLOSE,SHORT) - EMA(CLOSE,LONG);//短周期与长周期的收盘价的指数平滑移动平均值做差。
-    // DEA  : EMA(DIFF,M);//DIFF的M个周期指数平滑移动平均
-    // 2*(DIFF-DEA),COLORSTICK;//DIFF减DEA的2倍画柱状线
+    //指标定义
     C.DEFINE({
         type: "SUB",
         cname: "MACD",
@@ -11,12 +9,12 @@ function* macd(C) {
         ]
     });
     //参数
-    let vshort = C.PARAM(20, "SHORT", {MIN: 5, STEP: 5});
-    let vlong = C.PARAM(35, "LONG", {MIN: 5, STEP: 5});
-    let vm = C.PARAM(10, "M", {MIN: 5, STEP: 5});
-    //输入
+    let vshort = C.PARAM(20, "SHORT", {min: 1, memo:"短周期"});
+    let vlong = C.PARAM(35, "LONG", {min: 2, memo:"长周期"});
+    let vm = C.PARAM(10, "M", {min: 1});
+    //输入序列
     let sclose = C.SERIAL("CLOSE");
-    //输出
+    //输出序列
     let diff = C.OUTS("LINE", "diff", {color: RED});
     let dea = C.OUTS("LINE", "dea", {color: BLUE, width: 2});
     let bar = C.OUTS("BAR", "bar", {color: RED});
