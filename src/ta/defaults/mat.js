8f1b8f5d36e59f14b16ecab6a939734243bc977b
yangyang <yangyang@shinnytech.com>
2018-04-07 09:26:50 +0800

    增加均线交易示例

---
+++ src/ta/defaults/mat.js
@@ -0,0 +1,27 @@
+function* mat(C){
+    //指标定义
+    C.DEFINE({
+        type: "MAIN",
+        cname: "均线交易策略演示",
+        state: "KLINE",
+    });
+    //参数
+    let n1 = C.PARAM(3, "N1");  //短均线周期
+    let n2 = C.PARAM(10, "N2"); //长均线周期
+    //输入序列
+    let c = C.SERIAL("CLOSE");
+    //输出序列
+    let m1 = C.OUTS("LINE", "ma" + n1, {color: RED});
+    let m2 = C.OUTS("LINE", "ma" + n2, {color: GREEN});
+    //计算
+    while(true) {
+        let i = yield;
+        m1[i] = MA(i, c, n1, m1);  //计算短均线值
+        m2[i] = MA(i, c, n2, m2);  //计算长均线值
+        if (m1[i] > m2[i] && m1[i-1] <= m2[i-1])  //短均线上穿长均线，买进
+          C.ORDER("BUY", "CLOSEOPEN", 1);
+        if (m1[i] < m2[i] && m1[i-1] >= m2[i-1])  //短均线下穿长均线，卖出
+          C.ORDER("SELL", "CLOSEOPEN", 1);
+    }
+}
+
