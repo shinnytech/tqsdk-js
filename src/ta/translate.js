dab1efe9d9e1ce4a9bbe37c7b1fce61aceffb705
mayanqiong <mayanqiong@shinnytech.com>
2018-04-09 16:12:30 +0800

    fix bug: 修改UI

--- src/ta/translate.js
+++ src/ta/translate.js
@@ -10,26 +10,29 @@ $(function () {
     result_editor.$blockScrolling = Infinity;
     result_editor.setReadOnly(true);
     // 参数区域
-    $('div.right-menu a.indicator-prop').on('click', function (e) {
-        $('div.right-menu button>span.show-prop').text(this.innerText);
+    $('div.left-menu a.indicator-prop').on('click', function (e) {
+        $('div.left-menu button>span.show-prop').text(this.innerText);
     });
-    $('div.right-menu table.wh-param-list').find('input').on('click', function (e) {
+    $('div.left-menu table.wh-param-list').find('input').on('click', function (e) {
         $(this).attr('readonly', false);
     });
 
     $('button#translate').on('click', function (e) {
-        $('#translate_status').attr('class', 'alert alert-info').text('翻译中......');
+        $('#tianqin #translate-status').attr('class', 'panel-heading').text('翻译中......');
+        $('#tianqin').attr('class', 'panel panel-info');
         var code = editor.getValue();
         var result = covertWHRequest(code)
             .then(response => response.json())
             .then(data => {
                 if (data.errline === 0) {
-                    $('#translate_status').attr('class', 'alert alert-success').text('翻译完成');
+                    $('#tianqin #translate-status').text('翻译完成');
+                    $('#tianqin').attr('class', 'panel panel-success');
                     result_editor.getSession().setValue(data.target, 1);
                 } else {
                     result_editor.getSession().setValue('', 1);
-                    var err = `error in ${data.errline}:${data.errcol} \n ${data.errvalue}`;
-                    $('#translate_status').attr('class', 'alert alert-danger').text(err);
+                    // var err = `error in ${data.errline}:${data.errcol} \n ${data.errvalue}`;
+                    $('#tianqin #translate-status').text('错误：' + data.errvalue).show();
+                    $('#tianqin').attr('class', 'panel panel-danger');
                 }
             })
             .catch(error => {
@@ -53,11 +56,11 @@ function covertWHRequest(code) {
         'K线附属指标': 'MAIN',
         '副图指标': 'SUB'
     }
-    let prop = $('div.right-menu button>span.show-prop').text();
+    let prop = $('div.left-menu button>span.show-prop').text();
     request_body.type = map_indicator_prop_to_type[prop];
 
     // params
-    let $trs = $('div.right-menu table.wh-param-list tbody tr');
+    let $trs = $('div.left-menu table.wh-param-list tbody tr');
     for (let i = 1; i <= 6; i++) {
         let name = $trs.find('.name_' + i).val().trim();
         if (name) {
