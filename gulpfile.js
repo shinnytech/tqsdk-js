8af5561611dcc628d112f5f72812eac28bb7f4aa
mayanqiong <mayanqiong@shinnytech.com>
2018-05-07 14:13:00 +0800

    fix bug: gulp打包输出到一个文件夹，简化输出内容

--- gulpfile.js
+++ gulpfile.js
@@ -17,85 +17,66 @@ var minimist = require('minimist');
 var minifyJs = composer(minifyJsEs6, console);
 var vString = '-v' + require('./package').version;
 
-var dist_ta = 'dist_ta/';
-var dist_tq = 'dist_tq/';
+var dist = 'dist/';
 
 // 获取命令行参数
 var argv = minimist(process.argv.slice(2));
 
 gulp.task('default', ['clean'], function () {
-    return gulp.start("indictor", 'trader');
+    return gulp.start("indictor", 'copy', 'minicss', 'minihtml', 'minijs');
 });
 
 /**
  * 清空目标文件夹
  */
 gulp.task('clean', function () {
-    return gulp.src([dist_ta, dist_tq]).pipe(clean());
+    return gulp.src([dist]).pipe(clean());
 });
 
-/**
- * ta/translate.html
- * ta/translate.js
- */
-gulp.task('translate.html', function () {
+gulp.task('minihtml', function () {
     return gulp.src(['./src/ta/translate.html'], { base: 'src' })
     .pipe(minifyHtml({ collapseWhitespace: true, removeComments: true }))
-        .pipe(gulp.dest(dist_ta));
+        .pipe(gulp.dest(dist));
 });
 
-gulp.task('translate.js', function () {
-    return gulp.src(['./src/ta/translate.js'], { base: 'src' })
+gulp.task('minijs', function () {
+    return gulp.src(['./src/ta/translate.js', './src/ta/worker.js'], { base: 'src' })
         .pipe(minifyJs())
-        .pipe(gulp.dest(dist_ta));
+        .pipe(gulp.dest(dist));
 });
 
-
+gulp.task('minicss', function () {
+    return gulp.src(['./src/ta/css/index.css', './src/tq.css'], { base: 'src' })
+        .pipe(minifyCss())
+        .pipe(gulp.dest(dist));
+});
 
 /**
  * ta/index.html
  */
-gulp.task('indictor', ['css'], function () {
-    delKeywordWarpContent('./src/ta/index.html', dist_ta + 'ta/index.html', 'del')
-    return gulp.src([dist_ta + 'ta/index.html'])
+gulp.task('indictor', ['indictorjs'], function () {
+    delKeywordWarpContent('./src/ta/index.html', dist + 'ta/index.html', 'del')
+    return gulp.src([dist + 'ta/index.html'])
         .pipe(minifyHtml({ collapseWhitespace: true, removeComments: true }))
-        .pipe(replace('index.js', 'index' + vString + '.js'))
-        .pipe(replace('index.css', 'index' + vString + '.css'))
-        .pipe(gulp.dest(dist_ta + 'ta/'));
+        .pipe(gulp.dest(dist + 'ta/'));
 });
 
-/**
- * copy css
- */
-gulp.task('css', ['js'], function () {
-    return gulp.src(['./src/ta/css/*.css'], { base: 'src' })
-        .pipe(concat('index' + vString + '.css'))
-        .pipe(minifyCss())
-        .pipe(gulp.dest(dist_ta + 'ta/css/'));
-});
-
-gulp.task('js', ['workerjs'], function () {
+gulp.task('indictorjs', function () {
     return gulp.src(['./src/ta/js/list_menu.js', './src/ta/js/utils.js', './src/ta/index.js'], { base: 'src' })
-        .pipe(concat('index' + vString + '.js'))
+        .pipe(concat('index.js'))
         .pipe(minifyJs())
-        .pipe(replace('js/worker.js', 'worker' + vString + '.js'))
-        .pipe(gulp.dest(dist_ta + 'ta/'));
+        .pipe(gulp.dest(dist + 'ta/'));
 });
 
-gulp.task('workerjs', ['copy_ta', 'translate.html', 'translate.js'], function () {
-    return gulp.src(['./src/ta/js/worker.js'])
-        .pipe(concat('worker' + vString + '.js'))
-        .pipe(minifyJs({}))
-        .pipe(gulp.dest(dist_ta + 'ta/'));
-});
-
-gulp.task('copy_ta', ['beforecopy'], function () {
+gulp.task('copy', [], function () {
     return gulp.src([
         './src/assets/jquery.min.js',
         './src/assets/noty.js',
         './src/assets/bootstrap/js/bootstrap.min.js',
         './src/assets/bootstrap/css/bootstrap.min.css',
         './src/assets/bootstrap/fonts/**',
+        './src/assets/highlight/**', //tq
+        './src/*.html', //tq
         './src/ta/ace-min/ace.js',
         './src/ta/ace-min/*-javascript.js',
         './src/ta/ace-min/ext-*.js',
@@ -103,84 +84,12 @@ gulp.task('copy_ta', ['beforecopy'], function () {
         './src/ta/ace-min/theme-*.js',
         './src/ta/ace-min/snippets/javascript.js',
         './src/ta/ace-min/snippets/text.js',
-        './src/ta/defaults/*',
         './src/libs/**',
         '!./src/libs/test/',
         '!./src/libs/test/*',
     ], { base: "src" })
-    .pipe(gulp.dest(dist_ta));;
+    .pipe(gulp.dest(dist));;
 });
-
-/**
- * 生成 defaults.json
- */
-gulp.task('beforecopy', function () {
-    var files = fs.readdirSync('./src/libs/ind/');
-    var obj = {};
-    files.forEach(function (filename) {
-        if (filename.endsWith('.js') && filename != 'basefuncs.js') {
-            var name = filename.substr(0, filename.length - 3);
-            var content = fs.readFileSync('./src/libs/ind/' + filename, 'utf8');
-            obj[name] = content.trim();
-        }
-    });
-    fs.writeFileSync('./src/libs/ind/defaults.json', JSON.stringify(obj), 'utf8');
-    return;
-});
-
-
-/**
- * trader.html + css + js
- */
-gulp.task('trader', ['traderjs', 'copy_tq'], function () {
-    var files = fs.readdirSync('./src/');
-    var obj = {};
-    files.forEach(function (filename) {
-        if (filename.endsWith('.html') && filename != 'example.ma.html')
-            delKeywordWarpContent('./src/' + filename, dist_tq + filename, 'concat');
-    });
-    return gulp.src(['./src/*.css'], { base: 'src' })
-        .pipe(minifyCss())
-        .pipe(gulp.dest(dist_tq));
-});
-
-gulp.task('traderjs', function () {
-    var filelist = [];
-    var reg = /src=\"(\S*)\"\>/;
-    var fileContent = fs.readFileSync('./src/example.html', 'utf8');
-    var start = false;
-    var lines = fileContent.split(/\r?\n/);
-    lines.forEach((ele, index, arr) => {
-        if (ele.includes('concat:start')) {
-            start = true;
-        } else if (ele.includes('concat:end')) {
-            start = false;
-        } else if (start) {
-            var matchRes = ele.match(reg);
-            if (matchRes) filelist.push('./src/' + matchRes[1]);
-        }
-    });
-    return gulp.src(filelist.concat('./src/tqsdk.js'))
-        .pipe(concat('tqsdk.js'))
-        .pipe(minifyJs({}))
-        .pipe(gulp.dest(dist_tq));
-});
-
-gulp.task('copy_tq', function () {
-    return gulp.src([
-        './src/assets/jquery.min.js',
-        './src/assets/noty.js',
-        './src/assets/bootstrap/js/bootstrap.min.js',
-        './src/assets/bootstrap/css/bootstrap.min.css',
-        './src/assets/bootstrap/fonts/**',
-        './src/assets/highlight/**',
-        './src/libs/**',
-        '!./src/libs/test/',
-        '!./src/libs/test/*',
-    ], { base: "src" })
-        .pipe(gulp.dest(dist_tq));
-});
-/** trader.html + css + js */
 
 gulp.task('localRun', function () {
     connect.server({
