1b3a43db015d89b6927957209b4968de78a70203
mayanqiong <mayanqiong@shinnytech.com>
2018-05-15 15:15:44 +0800

    add: function ROUND

--- src/libs/func/basefuncs.js
+++ src/libs/func/basefuncs.js
@@ -19,7 +19,7 @@ function COUNT(i, serial, n, cache) {
 }
 
 function REF(i, serial, n) {
-    return serial[i-n];
+    return serial[i - n];
 }
 
 function MA(i, serial, n, cache) {
@@ -37,7 +37,7 @@ function EMA(i, serial, n, cache) {
 function DMA(i, serial, a, cache) {
     if (cache.length == 0)
         return serial[i];
-    return isNaN(cache[i - 1]) ? serial[i] : (cache[i - 1] * (1-a) + serial[i] * a);
+    return isNaN(cache[i - 1]) ? serial[i] : (cache[i - 1] * (1 - a) + serial[i] * a);
 }
 
 function SMA(i, serial, n, m, cache) {
@@ -70,11 +70,11 @@ function STDEV(i, serial, n, cache) {
     cache._s = cache._s ? cache._s : [];
     let x2 = 0;
     let x = 0;
-    if (cache._s[i-1]){
+    if (cache._s[i - 1]) {
         x = cache._s[i - 1][0] - serial[i - n] + serial[i];
         x2 = cache._s[i - 1][1] - serial[i - n] * serial[i - n] + serial[i] * serial[i];
 
-    }else{
+    } else {
         for (let k = i - n + 1; k <= i; k++) {
             let d = serial[k];
             x2 += d * d;
@@ -84,7 +84,7 @@ function STDEV(i, serial, n, cache) {
 
     let s2 = x2 - x * x / n;
     // 小数精度问题 引起算出来的方差是一个很小的负数
-    if(s2 < 0 && s2 > -0.000001) s2 = Math.abs(s2);
+    if (s2 < 0 && s2 > -0.000001) s2 = Math.abs(s2);
 
     let n_ = n == 1 ? 1 : n - 1; // 标准差最后除以 n-1
     let std = Math.sqrt(s2 / n_);
@@ -95,10 +95,10 @@ function STDEV(i, serial, n, cache) {
 }
 
 function IFELSE(c, a, b) {
-    return c?a:b;
+    return c ? a : b;
 }
 
-function ABS(v){
+function ABS(v) {
     return Math.abs(v);
 }
 
@@ -110,18 +110,18 @@ function MIN(v1, v2) {
     return Math.min(v1, v2);
 }
 
-function TIME(dt_nano){
+function TIME(dt_nano) {
     let d = new Date(dt_nano / 1000000);
     let s = d.getHours() * 10000 + d.getMinutes() * 100 + d.getSeconds();
     return s;
 }
 
-function DATE(dt_nano){
+function DATE(dt_nano) {
     let d = new Date(dt_nano / 1000000);
     return d.getFullYear() * 10000 + (d.getMonth() + 1) * 100 + d.getDay();
 }
 
-function NEAREST(i, serial){
+function NEAREST(i, serial) {
     for (let j = i; j >= i - 100; j--) {
         if (serial[j])
             return j;
@@ -129,10 +129,18 @@ function NEAREST(i, serial){
     return NaN;
 }
 
-function EVERY(i, serial, n){
+function EVERY(i, serial, n) {
     for (let j = i; j >= i - n; j--) {
         if (!serial[j])
             return false;
     }
     return true;
+}
+
+function ROUND(number, precision) {
+    var shift = function (number, precision) {
+        var numArray = ("" + number).split("e");
+        return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
+    };
+    return shift(Math.round(shift(number, +precision)), -precision);
 }
