071b3e56e41c01045e9897b47ea47a8b768aaeea
mayanqiong <mayanqiong@shinnytech.com>
2018-08-03 10:56:06 +0800

    add demo: drawline

--- src/libs/custom/drawline.js
+++ src/libs/custom/drawline.js
@@ -4,11 +4,11 @@ function* drawline (C) {
         type: "MAIN", // 指标类型, SUB=副图指标, MAIN=主图指标
     });
 
-    //TODO: 定义临时序列. 如果技术指标需要临时序列, 请在这里按下面的例子定义:
-    let id = 'line1';
-
     while(true) {
         let i = yield;
-        C.DRAW_LINE(id, i-20, C.DS.close[i-100], i, C.DS.high[i]);
+         C.DRAW_LINE('line1', i-20, C.DS.close[i-20], i, C.DS.high[i], WHITE, 2, 0);
+         C.DRAW_RAY('line2', i-20, C.DS.close[i-20], i, C.DS.high[i], YELLOW, 2, 0);
+         C.DRAW_SEG('line3', i-20, C.DS.close[i-20], i, C.DS.high[i], RED, 1, 0);
+        
     }
 }
