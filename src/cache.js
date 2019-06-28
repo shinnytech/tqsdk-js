75e774e16c796e05d41c74f7c5eea8280b219833
Elizabeth <ma_yanqiong@163.com>
2019-06-28 18:06:07 +0800

    fix bug: delete old version database

--- src/cache.js
+++ src/cache.js
@@ -1,6 +1,20 @@
 import localForage from "localforage"
+import * as project_package from '../package.json'
 const DB_NAME = 'his_settlements';
 let stores = {};
+let version = project_package.db_version
+let old_version = localStorage.getItem('cc_db_ver')
+
+if (old_version != version) {
+  // 数据库版本升级，整个数据库重置
+  let DBDeleteRequest = indexedDB.deleteDatabase(DB_NAME);
+  DBDeleteRequest.onerror = function(event) {
+    console.log("Error deleting database.");
+  };
+  DBDeleteRequest.onsuccess = function(event) {
+    localStorage.setItem('cc_db_ver', version)
+  };
+}
 
 export default {
   getContent(user_id, trading_day){
