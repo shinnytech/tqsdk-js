7d48ec2a3d23937d703bfad6db90e51b1b3c31ef
shinny-mayanqiong <mayanqiong@shinnytech.com>
2020-11-25 22:48:53 +0800

    fix check-version

--- check-version.js
+++ check-version.js
@@ -1,13 +1,13 @@
 const core = require('@actions/core');
 const request = require('request');
-const this_version = require('./package.json').version;
+const packageJson = require('./package.json');
+
 
 // 如果当前版本号 > npm 远程库版本号，则发布新版本到 npm
 try {
-    request.get("https://registry.npmjs.org/tqchart", function(error, response, body) {
+    request.get("https://registry.npmjs.org/" + packageJson.name, function(error, response, body) {
         const version = JSON.parse(body)['dist-tags'].latest
-        console.log(this_version, version, this_version > version)
-        if (this_version > version) {
+        if (packageJson.version > version) {
             core.setOutput("should_publish", "1");
         } else {
             core.setOutput("should_publish", "0");
